2a4b7ff1b1f00f8b80e603883552da4c2954cfab
Brandan Richardson <brandanr@gmail.com>
2018-05-19 00:07:56 -0700

    made color inputs match the background generated on the first page load.

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient');
 
+function startGradient() {
+    color1.setAttribute('value', '#ff0000');
+    color2.setAttribute('value', '#ffff00');
+    
+    
+}
+
 function setGradient() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
     css.textContent = body.style.background + ";"
 }
+
+startGradient();
 
 color1.addEventListener('input', setGradient);
 
